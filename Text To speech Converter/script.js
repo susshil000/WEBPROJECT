0e5dab11fd13fa80e3e99c2aa0eaa76f33ed6933
susshil000 <sushilyadav20065@gmail.com>
2026-05-18 22:04:41 +0530

    Added new project

---
+++ Text To speech Converter/script.js
@@ -0,0 +1,19 @@
+let speech = new SpeechSynthesisUtterance();
+let voices=[];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    voiceSelect.innerHTML = voices
+        .map((voice, index) => `<option value="${index}">${voice.name} (${voice.lang})</option>`)
+        .join("");
+}
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
